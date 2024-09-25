7c46a147daba799372388f42ba307f5492d6ad71
harunaendo <163505612+harunaendo@users.noreply.github.com>
2024-09-25 10:27:06 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   get "/" =>'homes#top'
+
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts/new'
+  get 'posts/:id' => 'posts#show'
+
   get "about" => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
