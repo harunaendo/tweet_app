6b75fb2ac01c0c26481e2f4012770bed1ce332af
harunaendo <163505612+harunaendo@users.noreply.github.com>
2024-11-22 07:11:51 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
+  root to: "homes#top"
+  get "/" => "home#top"
+  get "about" => "home#about"
+
   post "users/create" => "users#create"
   get "signup" => "users#new"
   get "users/index" => "users#index"
   get "users/:id" => "users#show"
-
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
@@ -12,7 +16,5 @@ Rails.application.routes.draw do
   post "posts/:id/update" => "posts#update"
   get "posts/:id/destroy" => "posts#destroy" #postからgetに変更
 
-  get "/" => "home#top"
-  get "about" => "home#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
